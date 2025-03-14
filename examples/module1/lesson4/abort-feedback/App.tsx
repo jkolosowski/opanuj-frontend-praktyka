7714bf1717e96a49d84b9e58f8217b78b30e7a55
Jakub Kołosowski <kuba.kolosowski@icloud.com>
2025-03-14 11:17:11 +0100

    feat: add abort feedback - lesson 4

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 interface User {
@@ -9,27 +10,47 @@ const API_URL = '/api/data/users?timeout=10000';
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [isTimeoutError, setIsTimeoutError] = useState(false);
+
+  const fetchUsers = async () => {
+    await axios
+      .get<User[]>(API_URL, { timeout: 5000 })
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        if (error.code === 'ECONNABORTED') {
+          setIsTimeoutError(true);
+        }
+      });
+  };
+
+  const retryRequest = () => {
+    setIsTimeoutError(false);
+    fetchUsers();
+  };
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(({ users }) => {
-        setUsers(users);
-      });
+    fetchUsers();
   }, []);
 
   return (
     <div>
       <div className="flex flex-row items-center justify-between py-4">
         <h1 className="text-2xl font-bold">Users</h1>
-        <div className="flex flex-row items-center">
-          <p className="mr-2">
-            Sorry, there seems to be connectivity issues...
-          </p>
-          <button className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
-            Try again
-          </button>
-        </div>
+        {isTimeoutError && (
+          <div className="flex flex-row items-center">
+            <p className="mr-2">
+              Sorry, there seems to be connectivity issues...
+            </p>
+            <button
+              onClick={retryRequest}
+              className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4"
+            >
+              Try again
+            </button>
+          </div>
+        )}
       </div>
       <ul className="space-y-2">
         {users.map((user, index) => (
